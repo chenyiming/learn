543de3df52c30a12ebb8394b7232a9cd11d8c104
chenyiming <1985614@qq.com>
2016-08-31 12:41:46 +0800

    添加httpclient rest

---
+++ workspace-disc/itsm/web/src/test/java/com/future/rest/RestAdapter.java
@@ -0,0 +1,82 @@
+package com.future.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+
+public class RestAdapter {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		try {
+
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpGet getRequest = new HttpGet(
+				"http://192.168.12.19:443/819/json/pass_cdata_1.json");
+			getRequest.addHeader("accept", "application/json");
+
+			HttpResponse response = httpClient.execute(getRequest);
+
+			if (response.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+				   + response.getStatusLine().getStatusCode());
+			}
+
+			BufferedReader br = new BufferedReader(
+	                         new InputStreamReader((response.getEntity().getContent())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			httpClient.getConnectionManager().shutdown();
+
+		  } catch (ClientProtocolException e) {
+
+			e.printStackTrace();
+
+		  } catch (IOException e) {
+
+			e.printStackTrace();
+		  }
+		}
+
+}
