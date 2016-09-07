968dd4c5ba642914df4f9a7dc113bcad27f9e628
chenyiming <1985614@qq.com>
2016-09-07 13:33:02 +0800

    删除提交错误的代码

--- workspace-disc/itsm/elasticsearch/src/main/java/org/springframework/data/elasticsearch/repositories/SampleArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.springframework.data.elasticsearch.repositories;
-
-import org.springframework.data.elasticsearch.entities.Article;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface SampleArticleRepository extends ElasticsearchRepository<Article,String> {
-}
