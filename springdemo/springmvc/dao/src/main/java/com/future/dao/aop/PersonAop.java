ba91fcc57e5e144f24c4a353d01b8b91bd9ad1d8
陈珂 <1985614@qq.com>
2016-11-20 21:57:56 +0800

    添加aop机制

---
+++ springdemo/springmvc/dao/src/main/java/com/future/dao/aop/PersonAop.java
@@ -0,0 +1,31 @@
+package com.future.dao.aop;
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;  
+import org.aspectj.lang.annotation.AfterReturning;  
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.stereotype.Component;  
+import org.springframework.util.StringUtils;  
+
+@Aspect //注解定义切面
+@Component
+public class PersonAop {
+
+	@Before("execution (* com.future.dao.PersonDao.save(..))")  
+	 public void beforeMethod(JoinPoint joinPoint){
+		 String methodName = joinPoint.getSignature().getName();
+	        Object [] args = joinPoint.getArgs();
+	        
+	        System.out.println("The method " + methodName + " begins with " + Arrays.asList(args));
+	}
+	
+	
+	@AfterReturning(value="execution (* com.future.dao.PersonDao.save(..))",
+            returning="result")
+    public void afterReturning(JoinPoint joinPoint, Object result){
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("The method " + methodName + " ends with " + result);
+    }
+}
